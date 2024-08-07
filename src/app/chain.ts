c2e900813cb7572f3e72f9966c25fd67b76980d4
masysz <67843831+masysz@users.noreply.github.com>
2024-08-07 10:15:14 +0700

    Update chain.ts

--- src/app/chain.ts
+++ src/app/chain.ts
@@ -1,4 +1,4 @@
 import { defineChain } from "thirdweb";
-// import { Base } from "thirdweb/chains";
+// import { base } from "thirdweb/chains";
 
-export const chain = defineChain( "Base" );
+export const chain = defineChain( "base" );
