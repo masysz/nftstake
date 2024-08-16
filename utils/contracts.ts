a582af39d92e87bb504760041536d0cc902102f6
masysz <67843831+masysz@users.noreply.github.com>
2024-08-16 08:39:32 +0700

    Update contracts.ts

--- utils/contracts.ts
+++ utils/contracts.ts
@@ -5,7 +5,7 @@ import { stakingABI } from "./stakingABI";
 
 const nftContractAddress = "0xb014519e5e9b7FBA10E27e21306f786C7B324952";
 const rewardTokenContractAddress = "0xfFbC9EfaAc969C89F1036b52192c224bA56F7Ef5";
-const stakingContractAddress = "0x7d244aBb53E3D2b57be75Bc162BB265a84484886";
+const stakingContractAddress = "0x2Ee2CBbfDc5FBe873FEDF5519B54e377389713f2";
 
 export const NFT_CONTRACT = getContract({
     client: client,
