cf38af21387512c2e28de0b4f700c387a7e88047
masysz <67843831+masysz@users.noreply.github.com>
2024-08-16 08:33:38 +0700

    Update NFTCard.tsx

--- components/NFTCard.tsx
+++ components/NFTCard.tsx
@@ -3,7 +3,7 @@ import { NFT, prepareContractCall } from "thirdweb";
 import { MediaRenderer, TransactionButton } from "thirdweb/react";
 import { NFT_CONTRACT, STAKING_CONTRACT } from "../utils/contracts";
 import { useState } from "react";
-import { approve } from "thirdweb/extensions/erc721";
+import { approve } from "thirdweb/extensions/erc1155";
 
 type OwnedNFTsProps = {
     nft: NFT;
